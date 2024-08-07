ec2662acd8f2afd1bdc7d583c574a85041afee33
onur <onurb215@hotmail.com>
2024-08-07 20:25:34 +0300

    const

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("sdgsdg");
+//Selectors 
+
+//Ekle formu
+
+const ekleBtn = document.querySelector("#ekle-btn")
+const gelirInput = document.querySelector("#gelir-input")
+const ekleFormu = document.querySelector("#ekle-formu")
+
+//Sonuç tablosu
+
+const gelirinzTable = document.getElementById("geliriniz")
+const giderinizTable = document.getElementById("gideriniz")
+const kalanTable = document.getElementById("kalan")
